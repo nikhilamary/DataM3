005302ae2dd403ab9a8f128fb1cbf641cbd10165
nikhila appachan <nikhilamary7@gmail.com>
2019-08-11 22:50:28 -0400

    Initial check-in of module DataManagement3

---
+++ DataManagement3/EmptyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DataManagement3
+{
+    public class EmptyClass
+    {
+        public class New
+        {
+
+
+            public string title { get; set; }
+            public string classes { get; set; }
+        }
+    }
+}
